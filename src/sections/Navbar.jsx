e5532f5bd7054290c1c210ca2f4f28626cd9dc00
VISHESH R S <vishesh2623@.com>
2026-08-09 21:24:02 +0530

    Updated code and features

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
             href="/"
             className="text-xl font-bold transition-colors text-neutral-400 hover:text-white"
           >
-            Mathesh
+            Vishesh R S 
           </a>
           <button
             onClick={() => setIsOpen(!isOpen)}
