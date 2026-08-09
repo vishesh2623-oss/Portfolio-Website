facc515648dcaac908b9e57b8fbe0cd9ef45d5f9
VISHESH R S <vishesh2623@.com>
2026-08-09 15:59:22 +0530

    Fix build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Navbar from "./sections/Navbar";
-import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Projects from "./sections/Projects";
 import Credentials from "./sections/Credentials";
